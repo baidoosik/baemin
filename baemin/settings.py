7e202bb29c4bf857b7b16248fb6ef693c9891378
baidoosik <qoentlr37@naver.com>
2017-07-14 02:02:32 +0900

    django shop app 추가하고 install 앱에 추가

--- baemin/settings.py
+++ baemin/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'shop'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'baemin.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templatesg'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
