d32659aa517fd7d6e19f99bd7445e68b594c3f2d
baidoosik <qoentlr37@naver.com>
2017-07-14 21:03:30 +0900

    회원가입 django auth앱의 usercreationform 이용해서 만들기

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url,include
-from django.contrib import admin
+from . import views
 from django.shortcuts import redirect
+from django.contrib.auth import views as auth_views
 
 urlpatterns =[
-    
+    url(r'^login/$',auth_views.login,name='login'),
+    url(r'^logout/$',auth_views.logout,name='logout'),
+    url(r'^signup/$',views.signup,name='signup'),
 ]
