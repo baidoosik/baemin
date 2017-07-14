97a2a1afd0c2fd8138fe67194174df15badc7986
baidoosik <qoentlr37@naver.com>
2017-07-14 21:10:08 +0900

    django auth 앱을 이용해서 login,logout 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,9 +2,10 @@ from django.conf.urls import url,include
 from . import views
 from django.shortcuts import redirect
 from django.contrib.auth import views as auth_views
+from django.conf import settings
 
 urlpatterns =[
-    url(r'^login/$',auth_views.login,name='login'),
-    url(r'^logout/$',auth_views.logout,name='logout'),
+    url(r'^login/$',auth_views.login,name='login',kwargs={'template_name': 'accounts/login.html'}),
+    url(r'^logout/$',auth_views.logout,name='logout', kwargs={'next_page': settings.LOGIN_URL}),
     url(r'^signup/$',views.signup,name='signup'),
 ]
